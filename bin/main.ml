64f027fecf8b0362819000905e4ee26a3cbdbf73
Christopher Fujino <christopherfujino@gmail.com>
2024-02-29 20:48:37 -0800

    clean up

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,6 @@
-open Gt.Git_foo
+open Gt
+open Main
+open Git_foo
 
 let programs =
   [
@@ -15,29 +17,29 @@ let () =
   let rev_value = Lwt_main.run rev_p in
   let hash = Store.Value.digest rev_value in
   let hash_str = Store.Hash.to_hex hash in
-  Hashtbl.replace identifiers "HEAD" (Gt.Interpreter.Commit hash_str)
+  Hashtbl.replace identifiers "HEAD" (Interpreter.Commit hash_str)
 
 let () =
   Hashtbl.replace identifiers "print"
     (Function
        (fun es ->
          match es with
-         | e :: [] -> StringValue (Gt.Interpreter.expr_to_string e)
-         | _ -> raise Foo));
+         | e :: [] -> StringValue (Interpreter.expr_to_string e)
+         | _ -> raise YoloDawg));
   let rev_p = get_head () in
   let rev_value = Lwt_main.run rev_p in
   let hash = Store.Value.digest rev_value in
   let hash_str = Store.Hash.to_hex hash in
   Hashtbl.replace identifiers "HEAD"
     (Function
-       (fun es -> match es with [] -> StringValue hash_str | _ -> raise Foo))
+       (fun es -> match es with [] -> StringValue hash_str | _ -> raise YoloDawg))
 
 let () =
   List.iter
     (fun program ->
-      let expr = Gt.Main.parse program in
+      let expr = parse program in
       Printf.printf "%s\n" program;
-      let open Gt.Interpreter in
+      let open Interpreter in
       interpret { identifiers } expr;
       print_endline "")
     programs
--- lib/git_foo.ml
+++ lib/git_foo.ml
@@ -1,7 +1,7 @@
+open Main
+
 module Store = Git_unix.Store
 module Search = Git.Search.Make (Digestif.SHA1) (Store)
-
-exception Foo
 
 let store =
   let store_p = Store.v (Fpath.v (Sys.getcwd ())) in
@@ -28,10 +28,10 @@ and get_parent (store_val : Store.hash) : Search.hash option =
   let val_res_prom = Store.read store store_val in
   let val_res = Lwt_main.run val_res_prom in
   let store_val = Result.get_ok val_res in
-  let commit = match store_val with Commit hash -> hash | _ -> raise Foo in
+  let commit = match store_val with Commit hash -> hash | _ -> raise YoloDawg in
   let parents = Store.Value.Commit.parents commit in
   (* TODO handle multiple parents *)
   match parents with
   | [] -> None
   | h :: [] -> Some h
-  | _ :: _ -> raise Foo
+  | _ :: _ -> raise YoloDawg
