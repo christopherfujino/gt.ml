dfe745dccd457039a4d3c48770a59c40c449d343
Christopher Fujino <christopherfujino@gmail.com>
2024-04-21 20:18:50 -0700

    cleanup

--- bin/main.ml
+++ bin/main.ml
@@ -12,24 +12,27 @@ let programs =
     {|print("Hello, world!")|};
   ]
 
-let () =
-  let identifiers =
+let identifiers =
+  let identifiers' =
     (* random is an optional, named parameter *)
     let random = false in
     Hashtbl.create ~random 20
   in
-  Hashtbl.replace identifiers "print"
+  Hashtbl.replace identifiers' "print"
     (Runtime.Function
        (fun es ->
          match es with
          | e :: [] -> String (Ast.to_string e)
          | _ -> raise Unreachable));
-  Hashtbl.replace identifiers "HEAD"
+  Hashtbl.replace identifiers' "HEAD"
     (Function
        (fun es ->
          match es with
          | [] -> Commit (() |> get_head |> Commit_runtime.commit_of_store_value)
          | _ -> raise (YoloDawg "wrong number of arguments")));
+  identifiers'
+
+let () =
   List.iter
     (fun program ->
       let expr = parse program in
