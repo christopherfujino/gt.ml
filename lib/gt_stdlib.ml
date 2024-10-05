c37f5642962c36809cb3e7356ab30926a3aead4d
Christopher Fujino <christopherfujino@gmail.com>
2024-10-04 22:15:56 -1000

    clean up

--- lib/gt_stdlib.ml
+++ lib/gt_stdlib.ml
@@ -2,34 +2,6 @@ open Common
 
 type t = (string, Runtime.t) Hashtbl.t
 
-let create_2 git_foo =
-  let module Git_foo = (val git_foo : Git_foo.Git_type) in
-  let identifiers : t =
-    (* random is an optional, named parameter *)
-    (* TODO: use Hashtbl.of_seq *)
-    Hashtbl.create ~random:false 20
-  in
-  Hashtbl.add identifiers "print"
-    (`Function
-      (fun es ->
-        match es with
-        | [ e ] ->
-            let contents =
-              match e with String s -> s | any -> Ast.to_string any
-            in
-            `String contents
-        | _ -> raise Unreachable));
-  Hashtbl.add identifiers "HEAD"
-    (`Function
-      (fun es ->
-        match es with
-        | [] ->
-            let head = Git_foo.get_head () in
-            let commit = Runtime.Commit.of_store_value head in
-            `Commit commit
-        | _ -> raise (YoloDawg "wrong number of arguments")));
-  identifiers
-
 let seq_of_list l =
   let rec f idx () =
     if idx >= List.length l then Seq.Nil else Cons (List.nth l idx, f (idx + 1))
